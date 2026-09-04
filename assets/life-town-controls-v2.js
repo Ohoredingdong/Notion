5134200eb81c20aa83963fb70f537caeb1804a82
Ohore <wldms3053@gmail.com>
2026-09-04 10:58:22 +0900

    Bump Work Today palette cache to v10

--- assets/life-town-controls-v2.js
+++ assets/life-town-controls-v2.js
@@ -9,7 +9,7 @@
     const wtStyleId='lt-work-today-palette-v3';
     if(!document.getElementById(wtStyleId)){
       const wt=document.createElement('link');
-      wt.id=wtStyleId;wt.rel='stylesheet';wt.href='./assets/work-today-palette-v3.css?v=20260903-9';
+      wt.id=wtStyleId;wt.rel='stylesheet';wt.href='./assets/work-today-palette-v3.css?v=20260904-10';
       document.head.appendChild(wt);
     }
   }
